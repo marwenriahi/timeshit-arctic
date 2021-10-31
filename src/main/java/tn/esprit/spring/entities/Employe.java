fd47e7da3a4f82b229c5b05c54522a868bb0f106
riadh <riad.amami@gmail.com>
2021-10-31 20:18:24 +0100

    add new stuff

--- src/main/java/tn/esprit/spring/entities/Employe.java
+++ src/main/java/tn/esprit/spring/entities/Employe.java
@@ -3,20 +3,17 @@ package tn.esprit.spring.entities;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
-import javax.validation.constraints.Pattern;
 
 import org.springframework.beans.factory.annotation.Value;
 
